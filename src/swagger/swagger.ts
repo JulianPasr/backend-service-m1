fd8c23a8408fec2b0b37e758ba48aea4d330ac23
JulianPasr <juliancolegiatura321@gmail.com>
2024-11-07 09:07:51 -0500

    conexion con el servidor y sondeo de errores

---
+++ src/swagger/swagger.ts
@@ -0,0 +1,24 @@
+import swaggerJsdoc, { Options } from "swagger-jsdoc";
+
+const swaggerOptions: Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Backend Service API",
+      version: "1.0.0",
+      description: "API para Catálogo de Productos"
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/"
+      }
+    ]
+  },
+  apis: [
+    "./src/routes/productRoutes.ts",
+  ]
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+export default swaggerSpec;
